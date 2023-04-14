cc8417436c34c2560200c32fa412dd49bc7d5882
Krajmark <kmark.mrkk@gmail.com>
2023-04-14 15:51:47 +0200

    initial

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+// hello.cpp
+// classic hello word style app
+// by Krajnyák Márk
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "ASDASD!" << endl;
+    /* code */
+    return 0;
+}
---
+++ name.h
@@ -0,0 +1,22 @@
+// name.h
+// by Krajnyák Márk
+
+#include <string>
+
+class Nagitme
+{
+private:
+    string first;
+    string last;
+public:
+    Name(/* args */);
+    ~Name();
+};
+
+Name::Name(/* args */)
+{
+}
+
+Name::~Name()
+{
+}
